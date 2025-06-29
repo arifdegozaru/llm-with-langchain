16a58ea0acf4e5bd74607ff2a8467037027b14ca
Ulik <arifdegozaru24@gmail.com>
2025-06-29 21:35:33 +0700

    linkedin_lookup_agent before building function for tools

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import Tool
+from langchain.prompts import PromptTemplate
+from langchain.tools import Tool
 from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
 
 load_dotenv()
 
@@ -24,4 +25,16 @@ def lookup(name: str) -> str:
         )
     ]
 
-    return "https://www.linkedin.com/in/arif-hidayatulah-4817a5a8"
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_url = result.output["output"]
+    return linkedin_url
+
+if __name__ == "__main__":
+    linkedin_url = lookup("Arif Hidayatullah")
--- ice_breaker.py
+++ ice_breaker.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
