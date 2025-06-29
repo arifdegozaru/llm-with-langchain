389f028a573c492b0b2244ce055c58459b98dda6
Ulik <arifdegozaru24@gmail.com>
2025-06-29 11:56:24 +0700

    chaining prompt_template with llm

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,9 +1,27 @@
 from dotenv import load_dotenv
 import os
 from langchain_core.prompts import PromptTemplate
-from langchain_openai
+from langchain_openai import ChatOpenAI
 
 if __name__ == "__main__":
     load_dotenv()
     print("Hello LangChain!")
-    print(os.environ['COOL_API_KEY'])
+    print(os.environ["COOL_API_KEY"])
+
+    summary_template = """
+        given the information {information} about a person from I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(api_key=os.environ["OPEN_AI_KEY"], model_name="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": "what is mitrais"})
+
+    print(res)
