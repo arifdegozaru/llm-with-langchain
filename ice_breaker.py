0afb11a6e228d006e2fd2fbf15dc503f6d9af45f
Ulik <arifdegozaru24@gmail.com>
2025-06-30 12:38:53 +0700

    add error handler if linkedin url not valid

--- ice_breaker.py
+++ ice_breaker.py
@@ -11,11 +11,13 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 def ice_break_with(name: str):
     linkedin_url = linkedin_lookup_agent(name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
+    print(linkedin_data)
 
     summary_template = """
         given the Linkedin information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        but if information detail not found or empty, just answer that you couldn't find about that person
     """
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
@@ -33,4 +35,4 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with("Kustian")
+    ice_break_with("arif hidayatullah")
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -10,30 +10,32 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     scrape information from LikedIn profiles,
     Manually scrape the information from the LinkedIn profile
     """
+    try:
+        if mock:
+            linkedin_profile_url = "https://gist.githubusercontent.com/arifdegozaru/db7e6caf407214411df64e6d45917dcc/raw/d05bde900f97243295be950e027cd46a299f73bf/arif-hidayatullah-scrapin.json"
+            response = requests.get(linkedin_profile_url, timeout=10)
+        else:
+            api_endpoint = "https://api.scrapin.io/enrichment/profile"
+            params = {
+                "apikey": os.environ["SCRAPIN_API_KEY"],
+                "linkedInUrl": linkedin_profile_url,
+            }
+            response = requests.get(
+                api_endpoint,
+                params=params,
+                timeout=10,
+            )
 
-    if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/arifdegozaru/db7e6caf407214411df64e6d45917dcc/raw/d05bde900f97243295be950e027cd46a299f73bf/arif-hidayatullah-scrapin.json"
-        response = requests.get(linkedin_profile_url, timeout=10)
-    else:
-        api_endpoint = "https://api.scrapin.io/enrichment/profile"
-        params = {
-            "apikey": os.environ["SCRAPIN_API_KEY"],
-            "linkedInUrl": linkedin_profile_url,
+        data = response.json().get("person")
+        data = {
+            k: v
+            for k, v in data.items()
+            if v not in ([], "", "", None) and k not in ["certifications"]
         }
-        response = requests.get(
-            api_endpoint,
-            params=params,
-            timeout=10,
-        )
 
-    data = response.json().get("person")
-    data = {
-        k: v
-        for k, v in data.items()
-        if v not in ([], "", "", None) and k not in ["certifications"]
-    }
-
-    return data
+        return data
+    except Exception:
+        return {}
 
 
 if __name__ == "__main__":
